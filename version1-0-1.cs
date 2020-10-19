25bb2b7841ad6777b7c6f7adfb8a57aceedd47ee
steamgamemodder <44922214+steamgamemodder@users.noreply.github.com>
2020-10-19 08:34:24 +0100

    1.0.1
    new version

---
+++ version1-0-1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace _1bitsound
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input Path:");
+            string pathin = Console.ReadLine();
+            Console.WriteLine("Output Path:");
+            string pathout = Console.ReadLine();
+            byte[] infile = File.ReadAllBytes(pathin);
+            int fileptr = 0;
+            for (fileptr = 0; fileptr < infile.Length; fileptr++)
+            {
+                infile[fileptr] = Convert.ToByte(infile[fileptr] & 0b10000000);
+            }
+            File.WriteAllBytes(pathout, infile);
+            Console.WriteLine("Done!");
+            Console.ReadKey();
+        }
+    }
+}
